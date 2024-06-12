9eb66676eda9e9070b43c805981dc981c0a9c018
devubu <ouf3vkw5a7ch1xt8@proton.me>
2024-06-12 18:59:16 -0400

    Custom Configuration

---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,82 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+
+    -- custom
+    "python",
+    "go",
+    "rust",
+    "cpp",
+    "bash",
+    "query",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+    -- shell stuff
+    "shfmt",
+
+    -- python stuff
+    "python-lsp-server",
+
+    -- rust stuff
+    "rust-analyzer",
+
+    -- go stuff
+    "gopls",
+
+    -- bash stuff
+    "bash-language-server",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
---
+++ lua/custom/mappings.lua
@@ -0,0 +1,31 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+
+    -- Tmux Navigation (Overrides NvChad's mappings)
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left"},
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down"},
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    }
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+-- more keybinds!
+
+return M
